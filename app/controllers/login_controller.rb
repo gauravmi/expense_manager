1f45bdd2a8247ac045d7c50dd5e43ecd3178d8c3
abinayasakthi <abinayasakthi@gmail.com>
2015-05-09 12:50:05 +0530

    Work on login page

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,13 +3,19 @@ class LoginController < ApplicationController
   end
 
   def create
-    user = validate params[:password],params[:username]
+    user = validate params[:login][:password], params[:login][:username]
     if user
       session[:id] = user.id
+      respond_to do |format|
+        format.html {redirect_to home_index_path, :notice=>'logged in successfully'}
+      end
+    else
+      render 'new'
     end
   end
-
   def validate(password, username)
+    p password
+    p username
     User.find_by_password_and_username(password, username)
   end
 end
