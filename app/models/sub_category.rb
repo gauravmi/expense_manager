36df5cd13d43b0569c2852eed3695ef40897d9b3
Gaurav <gmi3024@gmail.com>
2015-05-10 12:43:05 +0530

    last commit

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -2,7 +2,7 @@ class SubCategory < ActiveRecord::Base
 
   def self.fetch_all
     conn = PGconn.open(:dbname => 'expense_development')
-    res  = conn.exec("select name,sum(expense) from expenses JOIN sub_categories ON sub_categories.id = expenses.sub_cat_id where date_part('month', date) = #{Date.today.month} and date_part('year', date) = #{Date.today.year} group by name")
+    res  = conn.exec("select name, sum(expense) from expenses JOIN sub_categories ON sub_categories.id = expenses.sub_cat_id where date_part('month', date) = #{Date.today.month} and date_part('year', date) = #{Date.today.year} group by name")
     resultant = []
     res.each do |row|
       resultant << [row['name'].to_s,row['sum'].to_i]
