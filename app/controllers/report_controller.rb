9899a3ce2eed0f24b925eb3a5f80284262e6f489
abinayasakthi <abinayasakthi@gmail.com>
2015-05-10 08:26:57 +0530

    Date in the report view will make a difference

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -13,6 +13,8 @@ class ReportController < ApplicationController
     in_clause_for_sql += '\'Utilities\',' if !params["utilities_check"].nil?
     in_clause_for_sql += '\'Transportation\',' if !params["transportation_check"].nil?
     in_clause_for_sql += '\'Giving\',' if !params["giving_check"].nil?
+    month = params['report']['date(2i)'].to_i
+    year = params['report']['date(1i)'].to_i
 
     if in_clause_for_sql.end_with?(',')
       in_clause_for_sql = in_clause_for_sql[0..-2]
@@ -23,7 +25,7 @@ class ReportController < ApplicationController
     end
 
     @resultant = []
-    give_sql_result(to_be_put_in_sql).each do |row|
+    give_sql_result(to_be_put_in_sql, year, month).each do |row|
       @resultant << [row['cat_name'].to_s, row['expense_sum'].to_i]
     end
 
@@ -34,11 +36,11 @@ class ReportController < ApplicationController
     end
   end
 
-  def give_sql_result(to_be_put_in_sql)
+  def give_sql_result(to_be_put_in_sql, year, month)
     conn = PGconn.open(:dbname => 'expense_development')
     res = conn.exec("select mc.category_name as cat_name,sum(expense) as expense_sum from expenses e join sub_categories sc on(e.sub_cat_id = sc.id)
-  join main_categories mc on (sc.main_cat_id = mc.id) where date_part('month',e.date) = 12
-  and date_part('year',e.date) = 2014 #{to_be_put_in_sql} group by mc.category_name")
+  join main_categories mc on (sc.main_cat_id = mc.id) where date_part('month',e.date) = #{month}
+  and date_part('year',e.date) = #{year} #{to_be_put_in_sql} group by mc.category_name")
     res
   end
 
